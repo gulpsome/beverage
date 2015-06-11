06848d4552c22236432410536da5fc2235ff3da8
Orlin M Bozhinov <om@soundsapiens.com>
2015-06-11 22:09:21 +0300

    try global gulp before fallback to beverage's gulp

--- cli.js
+++ cli.js
@@ -6,8 +6,8 @@ var fs = require('fs')
 var path = require('path')
 var gulp = [
   path.normalize(process.cwd() + '/node_modules/.bin/gulp'),
-  path.normalize(process.cwd() + '/node_modules/beverage/node_modules/gulp/bin/gulp.js'),
-  path.join(path.dirname(fs.realpathSync(__filename)), '/gulp')
+  path.join(path.dirname(fs.realpathSync(__filename)), '/gulp'),
+  path.normalize(process.cwd() + '/node_modules/beverage/node_modules/gulp/bin/gulp.js')
 ]
 var found = false
 
@@ -18,8 +18,8 @@ gulp.forEach(function(file) {
   try {
     if (!found) {
       //console.log('trying', file)
-      require(file)
-      found = true
+      require(file) // delegate to gulp
+      found = true // break
     }
   }
   catch (e) {} // next

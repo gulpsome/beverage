b72de9ca42d3a708b66c2440132f806618c526ea
Orlin M Bozhinov <om@soundsapiens.com>
2015-06-12 00:09:07 +0300

    better forEach / comments

--- cli.js
+++ cli.js
@@ -19,21 +19,21 @@ if (beverage ||
     tasks.length === 0 ||
     R.contains('beverage', tasks) ||
     R.contains('help', tasks)) {
-  // this is a silent gulp
-  process.argv.push('--silent')
+  process.argv.push('--silent') // this is a silent gulp
   // TODO: insert beverage before --options to make gulp happy
   // if (beverage && ! R.contains('beverage', tasks)) process.argv.push('beverage')
 }
 
 gulp.forEach(function(file) {
-  try {
-    if (!found) {
+  if (!found) {
+    // does loop through all of it anyway
+    try {
       //console.log('trying', file)
       require(file) // delegate to gulp
-      found = true // break
+      found = true // break (as if)
     }
+    catch (e) {} // next (equivalent noop / required catch)
   }
-  catch (e) {} // next
 })
 
 if (!found) {
